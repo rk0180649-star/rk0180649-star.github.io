d7edcc834636f25f886da0105e3a2e9f2836425c
Rupesh <rk0180649@gmail.com>
2026-03-18 11:19:43 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -5,3 +5,28 @@ function toggleMenu(){
 function closeMenu(){
     document.getElementById("navbar-mobile").style.left="-300px"
 }
+
+function ChangeBG(color){
+    document.body.style.background = color;
+}
+let flag = 0;
+function controler(x){
+    flag = flag + x;
+    slideshow(flag);
+}
+slideshow(flag);
+
+ function slideshow(num){
+    let slides = document.getElementsByClassName('slide');
+    if(num == slides.length){
+        flag = 0;
+        num = 0;
+    }if(num < 0){
+        flag = slides.length-1;
+        num = slides.length-1;
+
+    }for(let y of slides){
+        y.style.display = 'none';
+    }
+    slides[num].style.display = "block";
+ }
